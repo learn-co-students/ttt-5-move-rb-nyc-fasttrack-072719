550e5830c4c7fd79c87f124ac241234a777dd874
Karina Macancela <karymacancela@gmail.com>
2019-07-28 18:03:47 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# convert String input to integer data type
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
+end
+
+# will add the character to board at specified index. X will be passed by default if no character is entered as an argument
+def move(board,index,character="X")
+  board[index] = character
+  return board
+end
